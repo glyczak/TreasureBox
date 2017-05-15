180de0e71b9962c30ca3fee2cf4272b81fc3fb9a
glyczak <gaba@gaba.space>
2017-05-15 09:37:58 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: glyczak
+ * Date: 5/15/17
+ * Time: 9:29 AM
+ */
+?>
+
+<html>
+<head>
+    <title>Welcome to TreasureBox</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<h1>It works!</h1>
+</body>
+</html>
